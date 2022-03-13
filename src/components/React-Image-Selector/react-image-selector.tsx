31a72b8b9191dcba0fc23874924fa63c3eac529c
Michael Brown <brownieboy@gmail.com>
2022-03-13 20:51:23 +1100

    chore: selectedImages can be array of ImageType

--- src/components/React-Image-Selector/react-image-selector.tsx
+++ src/components/React-Image-Selector/react-image-selector.tsx
@@ -7,12 +7,21 @@ import type { ImageType } from "./image";
 
 export type { ImageType as RISImageType };
 
+// function checkIsArrayString(x) {
+//   return x.every((i) => typeof i === "string");
+// }
+
 export const calculateCurrentSelections = (
   toggledImage: string | ImageType,
-  selectedImageValues: string[]
+  selectedImages: string[] | ImageType[]
 ) => {
   const valueToCheck =
     typeof toggledImage === "string" ? toggledImage : toggledImage?.value;
+
+  const selectedImageValues = selectedImages.map((image) =>
+    // @ts-ignore
+    image.value ? image.value : image
+  );
 
   if (selectedImageValues.includes(valueToCheck)) {
     return selectedImageValues.filter(
@@ -31,7 +40,7 @@ export interface ReactImageSelectorProps {
   /**
    * An array of currently selected image values
    */
-  selectedImageValues: string[];
+  selectedImages: string[];
   /**
    * If true, use checkboxes multi-select, otherwise radio single-select
    */
@@ -50,7 +59,7 @@ export interface ReactImageSelectorProps {
    */
   SelectorControl?: any;
   /**
-   * If any `selectedImageValues` are passed that are not in `images`, then this
+   * If any `selectedImages` are passed that are not in `images`, then this
    * prop will decide if a message is shown
    */
   warnIfImagesUnavailable?: boolean;
@@ -62,12 +71,16 @@ export interface ReactImageSelectorProps {
 export const ReactImageSelector: FunctionComponent<ReactImageSelectorProps> = ({
   availableImages,
   onPick,
-  selectedImageValues,
+  selectedImages,
   imageStyles,
   multiple = false,
   SelectorControl,
   warnIfImagesUnavailable = false,
 }) => {
+  // console.log(
+  //   "TCL ~ file: react-image-selector.tsx ~ line 84 ~ selectedImages, availableImages",
+  //   { selectedImages, availableImages }
+  // );
   const handleImageClick = (image: ImageType) => {
     if (typeof onPick === "function") {
       onPick(image);
@@ -76,9 +89,10 @@ export const ReactImageSelector: FunctionComponent<ReactImageSelectorProps> = ({
 
   const availableValuesArray = availableImages?.map((image) => image.value);
 
-  const selectedButUnavailable = selectedImageValues?.filter(
-    (imageValue) => !availableValuesArray?.includes(imageValue)
-  ) || [];
+  const selectedButUnavailable =
+    selectedImages?.filter(
+      (imageValue) => !availableValuesArray?.includes(imageValue)
+    ) || [];
 
   const renderUnavailable = () => {
     if (!warnIfImagesUnavailable || selectedButUnavailable.length === 0) {
@@ -101,7 +115,7 @@ export const ReactImageSelector: FunctionComponent<ReactImageSelectorProps> = ({
     return (
       <Image
         image={image}
-        isSelected={selectedImageValues?.includes(image.value) || false}
+        isSelected={selectedImages?.includes(image.value) || false}
         onImageClick={() => handleImageClick(image)}
         key={i}
         multiple={multiple}
