7b8fba6636e41e7d42fa5e68fec76229a39e8966
Michael Brown <brownieboy@gmail.com>
2022-03-20 18:10:33 +1100

    chore: checked tests passing

--- src/components/React-Image-Selector/react-image-selector.test.tsx
+++ src/components/React-Image-Selector/react-image-selector.test.tsx
@@ -8,25 +8,55 @@ import {
 
 import { availableImages } from "./test-data/availableImages.js";
 
-test("No props Renders an empty selector", () => {
-  render(
-    <ReactImageSelector />
-  );
+test("render a selector", () => {
+  render(<ReactImageSelector />);
 
   const picker = screen.getByRole("select");
   expect(picker).toBeInTheDocument();
 });
 
-test("With props, renders a selector with 4 images", () => {
-  render(
-    <ReactImageSelector availableImages={availableImages} selectedImages={[]} />
-  );
-
+test("renders 4 images in selector", () => {
+  render(<ReactImageSelector availableImages={availableImages} />);
   const images = screen.getAllByRole("img");
-  expect (images).toHaveLength(4);
-
+  expect(images).toHaveLength(4);
+  expect(images[3]).toBeInTheDocument();
 });
 
+test("renders 4 checkboxes, 'emperor' & 'king' selected", () => {
+  render(
+    <ReactImageSelector
+      availableImages={availableImages}
+      multiple
+      selectedImages={["emperor", "king"]}
+    />
+  );
+  const checkboxes = screen.getAllByRole("checkbox");
+  expect(checkboxes).toHaveLength(4);
+  expect(checkboxes[1]).toBeChecked(); // Emperor is 2nd element
+  expect(checkboxes[2]).toBeChecked(); // King is 3rd
+  expect(checkboxes[0]).not.toBeChecked();
+  expect(checkboxes[3]).not.toBeChecked();
+});
+
+test("renders 4 radios, 'emperor' selected", () => {
+  render(
+    <ReactImageSelector
+      availableImages={availableImages}
+      selectedImages={["emperor"]}
+    />
+  );
+  const radios = screen.getAllByRole("radio");
+  expect(radios).toHaveLength(4);
+  expect(radios[1]).toBeChecked(); // Emperor is 2nd element
+});
+
+// test("Calls ", () => {
+//   render(
+//     <ReactImageSelector availableImages={availableImages} selectedImages={[]} />
+//   );
+//   const images = screen.getAllByRole("img");
+//   expect (images).toHaveLength(4);
+// });
 
 /*
 import React from 'react';
